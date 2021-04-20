f022e54f227400c5baeda3b434c24bbb71f3fdd7
Fomagran <fomagran6@naver.com>
2021-04-20 19:03:41 +0900

    user 펑션 등록하기

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,22 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions'); 
+const admin = require('firebase-admin');
+const algoliasearch = require('algoliasearch');
+const ALGOLIA_APP_ID = functions.config().algolia.app;
+const ALGOLIA_ADMIN_KEY = functions.config().algolia.key;
+const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_ADMIN_KEY);
+const index = client.initIndex('users');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onCall((data, context) => {
+exports.onUsersCreated = functions.firestore.document('users/{userId}').onCreate((snap, context) => {
+  // Get the note document
+  const note = snap.data();
 
-  const text = data.text;
-  console.log("Text: " + text);
+  // Add an 'objectID' field which Algolia requires
+  note.objectID = context.params.noteId;
 
-  return {
-      message: text
-  }
+  // Write to the algolia index
+  const index = client.initIndex('users');
+  return index.saveObject(note);
 });
+
+
 
